2647e6f3d748ad086ba29509c389e3ac34de640c
Kudouhei <819457830@qq.com>
2018-02-18 17:04:12 +0800

    gh

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <pagecontainer />
+  </div>
+</template>
+
+<script>
+import Pagecontainer from './components/Pagecontainer'
+
+export default {
+  name: 'App',
+  components: {
+    Pagecontainer
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Pagecontainer.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div>{{this.dataList}}</div>
+    <Pagecomponent :total='this.totalPage'
+      @pageCallbackFn='getCurrentPage'/>
+  </div>
+</template>
+<script>
+import data from '../mock/data.json'
+import Pagecomponent from '../components/Pagecomponent'
+
+export default {
+  data() {
+    return {
+      dataList: [],
+      totalPage: data.length
+    }
+  },
+  components: {
+    Pagecomponent
+  },
+  methods: {
+    getCurrentPage(currentPage) {
+      this.dataList = data[currentPage-1].name;
+    }
+  }
+}
+</script>
+
